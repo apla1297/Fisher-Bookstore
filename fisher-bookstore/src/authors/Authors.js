055ec5ad73eb47b18dcf57f7fd41cf6e0f0df2f4
Andrew Pla <apla1297@gmail.com>
2020-02-05 23:12:09 -0500

    Authors Feature

---
+++ fisher-bookstore/src/authors/Authors.js
@@ -0,0 +1,14 @@
+import React from "react"; 
+import "./Authors.css"; 
+
+export default function Home()
+{
+   return (
+       <div classname = "Home">
+           <div className= "lander">
+               <h1>Authors</h1>
+               <p>Here are all the Authors!</p>
+           </div>
+       </div>
+   )
+}
